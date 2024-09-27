ba6fb57eb82a9ee21f1148444d4bfbfb0ccc2c98
Cae <2220840@ub.edu.ph>
2024-09-27 09:02:14 +0800

    working

--- app/src/main/java/com/example/recipeapp/MainActivity.java
+++ app/src/main/java/com/example/recipeapp/MainActivity.java
@@ -12,7 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int selectedTab = 1;
+    private int selectedTab = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,19 +20,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final LinearLayout homeLayout = findViewById(R.id.homeLayout);
-        final LinearLayout likeLayout = findViewById(R.id.likeLayout);
+        final LinearLayout NotifLayout = findViewById(R.id.NotifLayout);
         final LinearLayout addLayout = findViewById(R.id.addLayout);
         final LinearLayout userLayout = findViewById(R.id.userLayout);
+        final LinearLayout searchLayout = findViewById(R.id.searchLayout);
 
         final ImageView homeImage = findViewById(R.id.homeImage);
-        final ImageView likeImage = findViewById(R.id.NotifImage);
-        final ImageView addImage = findViewById(R.id.addImage);
+        final ImageView notifImage = findViewById(R.id.NotifImage);
+        final ImageView addImage = findViewById(R.id.AddImage);
         final ImageView userImage = findViewById(R.id.userImage);
+        final ImageView searchImage = findViewById(R.id.searchImage);
 
         final TextView homeText = findViewById(R.id.homeText);
-        final TextView likeText = findViewById(R.id.likeText);
+        final TextView notifText = findViewById(R.id.NotifText);
         final TextView addText = findViewById(R.id.addText);
         final TextView userText = findViewById(R.id.userText);
+        final TextView searchText = findViewById(R.id.searchText);
 
         // Load home fragment by default
         getSupportFragmentManager().beginTransaction()
@@ -55,20 +58,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        likeLayout.setOnClickListener(new View.OnClickListener() {
+        searchLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Check if home is already selected
                 if (selectedTab != 2) {
                     getSupportFragmentManager().beginTransaction()
                             .setReorderingAllowed(true)
-                            .replace(R.id.fragment_container_view_tag, LikesFragment.class, null)
+                            .replace(R.id.fragment_container_view_tag, searchFragment.class, null)
                             .commit();
-
-                    switchToTab(likeLayout, likeImage, likeText, R.drawable.notif, R.drawable.round_back_notif_100);
+                    switchToTab(searchLayout, searchImage, searchText, R.drawable.icon_search, R.drawable.round_back_search_100);
                     selectedTab = 2;
                 }
             }
         });
+
 
         addLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,17 +89,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        NotifLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Check if home is already selected
+                if (selectedTab != 4) {
+                    getSupportFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .replace(R.id.fragment_container_view_tag, NotifFragment.class, null)
+                            .commit();
+                    switchToTab(NotifLayout, notifImage, notifText, R.drawable.notif, R.drawable.round_back_notif_100);
+                    selectedTab = 4;
+                }
+            }
+        });
+
         userLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (selectedTab != 4) {
+                if (selectedTab != 5) {
                     getSupportFragmentManager().beginTransaction()
                             .setReorderingAllowed(true)
                             .replace(R.id.fragment_container_view_tag, userFragment.class, null)
                             .commit();
 
                     switchToTab(userLayout, userImage, userText, R.drawable.user, R.drawable.round_back_user_100);
-                    selectedTab = 4;
+                    selectedTab = 5;
                 }
             }
         });
@@ -125,18 +144,22 @@ public class MainActivity extends AppCompatActivity {
     private void resetTabs() {
         // Hide text and reset images for all tabs
         findViewById(R.id.homeText).setVisibility(View.GONE);
-        findViewById(R.id.likeText).setVisibility(View.GONE);
+        findViewById(R.id.searchText).setVisibility(View.GONE);
         findViewById(R.id.addText).setVisibility(View.GONE);
+        findViewById(R.id.NotifText).setVisibility(View.GONE);
         findViewById(R.id.userText).setVisibility(View.GONE);
 
         ((ImageView) findViewById(R.id.homeImage)).setImageResource(R.drawable.home);
-        ((ImageView) findViewById(R.id.addImage)).setImageResource(R.drawable.add);
+        ((ImageView) findViewById(R.id.searchImage)).setImageResource(R.drawable.icon_search);
+        ((ImageView) findViewById(R.id.AddImage)).setImageResource(R.drawable.add);
+        ((ImageView) findViewById(R.id.NotifImage)).setImageResource(R.drawable.notif);
         ((ImageView) findViewById(R.id.userImage)).setImageResource(R.drawable.user);
 
         // Reset background for all layouts
         findViewById(R.id.homeLayout).setBackgroundColor(getResources().getColor(android.R.color.transparent));
-        findViewById(R.id.likeLayout).setBackgroundColor(getResources().getColor(android.R.color.transparent));
+        findViewById(R.id.searchLayout).setBackgroundColor(getResources().getColor(android.R.color.transparent));
         findViewById(R.id.addLayout).setBackgroundColor(getResources().getColor(android.R.color.transparent));
+        findViewById(R.id.NotifLayout).setBackgroundColor(getResources().getColor(android.R.color.transparent));
         findViewById(R.id.userLayout).setBackgroundColor(getResources().getColor(android.R.color.transparent));
     }
 }
--- app/src/main/java/com/example/recipeapp/NotifFragment.java
+++ app/src/main/java/com/example/recipeapp/NotifFragment.java
@@ -1,6 +1,60 @@
 package com.example.recipeapp;
 
-import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
 
-public class NotifFragment extends Activity {
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NotifFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NotifFragment extends Fragment {
+
+    // Parameter arguments
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // Parameters
+    private String mParam1;
+    private String mParam2;
+
+    public NotifFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment notifFragment.
+     */
+    public static NotifFragment newInstance(String param1, String param2) {
+        NotifFragment fragment = new NotifFragment(); // Corrected to use notifFragment
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notif, container, false); // Correct layout reference
+    }
 }
